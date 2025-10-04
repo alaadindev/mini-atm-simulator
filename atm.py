af1ff29557b5309c620290694b692c89df69ad43
alaadev <alaadindev@gmail.com>
2025-10-04 13:40:18 +0300

    change all prints into format type for ease of use

--- atm.py
+++ atm.py
@@ -1,24 +1,33 @@
 #display main menu
 balance = 1000
 def main_menu():
+    global balance
     option = None
     while option != 4:
-        print(" 1. Check Balance\n", "2. Deposit Money\n", "3. Withdraw Money\n", "4. Quit")
+        print(f"\n1. Check Balance\n2. Deposit Money\n3. Withdraw Money\n4. Quit\n")
         option = int(input(""))
+        if option == 1:
+            print(f"your balance is: {balance}")
         if option == 2 or option == 3:
             exchange_menu(option)
+    print("GoodBye")
+
 def exchange_menu(option): # 1 dynamic function for both deposit and withdraw
-    print(" choose an option:", option)
+    print(f"\nchoose an option: {option}")
     options = ["Check Balance", "Deposit", "Withdraw", "Quit"]
-    amount = int(input(f" Enter amount to {options[option]}: ")) # -1 to turn it into index for our array
+    amount = int(input(f"Enter amount to {options[option-1]}: ")) # -1 to turn it into index for our array
     exchange(option, amount)
 
 def exchange(option, amount):
     global balance # turn out u can't access global var in python unless u used global keyword
     if option == 2:
         balance += amount
-    if option == 3 and (balance - amount) >= 0:
-        balance -= amount
-        print("Withdrawl successful. New balance:", balance)
+        print(f"Widthrawl successful. New Balance: {balance}")
+    if option == 3:
+        if (balance - amount) < 0:
+            print(f"Insuffcient funds!")
+        else:
+            balance -= amount
+            print(f"Withdrawl successful. New balance: {balance}\n")
 
 main_menu()
